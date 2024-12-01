c1acba586951e73ee08cd882feb40549977f6328
Tyler Hughes <tyler@tylerhughes.info>
2024-12-01 01:24:02 -0600

    chore: polished up the Bolt sidebar

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <div className="fixed bottom-0 left-0 p-2">
+      <button className="flex items-center text-bolt-elements-textSecondary bg-transparent hover:text-bolt-elements-textPrimary rounded-md p-1">
+        <div className="i-ph:sidebar-simple-duotone text-xl" />
+      </button>
+    </div>
+  );
+}
