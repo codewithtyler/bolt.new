e51b3f8721f87487eae83b3fafb47b14cd915de3
Nickolas Neal <nickneal17@gmail.com>
2024-12-08 18:05:18 -0500

    Added rays to UI

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -11,11 +11,10 @@ export function Header() {
   return (
     <header
       className={classNames(
-        'flex items-center bg-bolt-elements-background-depth-1 p-5 border-b h-[var(--header-height)]',
+        'flex items-center bg-transparent p-5 h-[var(--header-height)]',
         {
-          'border-transparent': !chat.started,
-          'border-bolt-elements-borderColor': chat.started,
-        },
+          'border-b border-bolt-elements-borderColor': chat.started,
+        }
       )}
     >
       <div className="flex items-center gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
